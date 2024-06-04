cf407dc902f169ccbfef42922f89930de383d425
CurLeeBrace <leeandreimain@gmail.com>
2024-06-04 11:15:56 +0800

    inc dashboard

--- client/src/Components/Table.tsx
+++ client/src/Components/Table.tsx
@@ -1,36 +1,41 @@
 import { Card, Typography } from "@material-tailwind/react";
+import { TableData } from "../routes/s-admin/DashBoard"; 
+const TABLE_HEAD = ["Municipality", "Co2e", "Ch4e", "N2o", "GHGe"];
  
-const TABLE_HEAD = ["Name", "Job", "Employed", ""];
+// const TABLE_ROWS = [
+//   {
+//     name: "John Michael",
+//     job: "Manager",
+//     date: "23/04/18",
+//   },
+//   {
+//     name: "Alexa Liras",
+//     job: "Developer",
+//     date: "23/04/18",
+//   },
+//   {
+//     name: "Laurent Perrier",
+//     job: "Executive",
+//     date: "19/09/17",
+//   },
+//   {
+//     name: "Michael Levi",
+//     job: "Developer",
+//     date: "24/12/08",
+//   },
+//   {
+//     name: "Richard Gran",
+//     job: "Manager",
+//     date: "04/10/21",
+//   },
+// ];
+
+
+interface TableProps  {
+  TABLE_ROWS : TableData[]
+}
  
-const TABLE_ROWS = [
-  {
-    name: "John Michael",
-    job: "Manager",
-    date: "23/04/18",
-  },
-  {
-    name: "Alexa Liras",
-    job: "Developer",
-    date: "23/04/18",
-  },
-  {
-    name: "Laurent Perrier",
-    job: "Executive",
-    date: "19/09/17",
-  },
-  {
-    name: "Michael Levi",
-    job: "Developer",
-    date: "24/12/08",
-  },
-  {
-    name: "Richard Gran",
-    job: "Manager",
-    date: "04/10/21",
-  },
-];
- 
-export default function Table() {
+export default function Table({TABLE_ROWS} : TableProps) {
   return (
     <Card className="h-full w-full overflow-scroll">
       <table className="w-full min-w-max table-auto text-left">
@@ -53,19 +58,19 @@ export default function Table() {
           </tr>
         </thead>
         <tbody>
-          {TABLE_ROWS.map(({ name, job, date }, index) => {
+          {TABLE_ROWS.map(({municipality, emission}, index) => {
             const isLast = index === TABLE_ROWS.length - 1;
-            const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
+            const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-100";
  
             return (
-              <tr key={name}>
+              <tr key={index}>
                 <td className={classes}>
                   <Typography
                     variant="small"
                     color="blue-gray"
                     className="font-normal"
                   >
-                    {name}
+                    {municipality}
                   </Typography>
                 </td>
                 <td className={classes}>
@@ -74,7 +79,7 @@ export default function Table() {
                     color="blue-gray"
                     className="font-normal"
                   >
-                    {job}
+                    {emission.co2e}
                   </Typography>
                 </td>
                 <td className={classes}>
@@ -83,7 +88,7 @@ export default function Table() {
                     color="blue-gray"
                     className="font-normal"
                   >
-                    {date}
+                    {emission.ch4e}
                   </Typography>
                 </td>
                 <td className={classes}>
@@ -94,7 +99,18 @@ export default function Table() {
                     color="blue-gray"
                     className="font-medium"
                   >
-                    Edit
+                    {emission.n2oe}
+                  </Typography>
+                </td>
+                <td className={classes}>
+                  <Typography
+                    as="a"
+                    href="#"
+                    variant="small"
+                    color="blue-gray"
+                    className="font-medium"
+                  >
+                    {emission.ghge}
                   </Typography>
                 </td>
               </tr>
