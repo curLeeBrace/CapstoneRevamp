53955e97eedb67aced683d3414228b58c09bf3d7
CurLeeBrace <leeandreimain@gmail.com>
2024-06-10 11:45:24 +0800

    config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,10 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
+  
   return {
     define: {
       'process.env': env
     },
     plugins: [react()],
+    server: {
+      open: true, // Automatically open the app in the default browser
+    },
   }
 })
