6ec78ee2f849ed527dcc4069c6eb9e5f838dd5a9
CurLeeBrace <leeandreimain@gmail.com>
2024-06-10 15:11:03 +0800

    congfig

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,19 +1,13 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-
+  
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  
   return {
     define: {
       'process.env': env
     },
     plugins: [react()],
-    // server: {
-    //   open: true, // Automatically open the app in the default browser
-    //   host : process.env.RAILWAY_PUBLIC_DOMAIN || "localhost",
-      
-    // },
   }
 })
