ac136a93f21c854979d5c687155f275ca2b1ab0d
Programmingwithmors <gjohnmoris@gmail.com>
2024-06-12 08:35:46 +0800

    put some log

--- server/src/middleware/handle_upload.ts
+++ server/src/middleware/handle_upload.ts
@@ -8,7 +8,7 @@ const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         
         console.log("Req : ", req.body)
-        return cb(null, `../client/public/img/user_img/${req.body.user_type}`);
+        return cb(new Error("Path Error!!"), `../client/public/img/user_img/${req.body.user_type}`);
       
     },
     filename: function (req, file, cb) {
