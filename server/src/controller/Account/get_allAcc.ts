60871ae1b3e229774d72f23b78dee26293367309
Mark Angelo <markangelofigueroa7@gmail.com>
2024-06-13 10:00:41 +0800

    fetching img

--- server/src/controller/Account/get_allAcc.ts
+++ server/src/controller/Account/get_allAcc.ts
@@ -1,31 +1,46 @@
 import { Request, Response } from "express";
 import AccountSchema from '../../db_schema/AccountSchema';
+import path from 'path';
+import fs from 'fs';
 
 const get_allAcc = async (req: Request, res: Response): Promise<Response> => {
   try {
     const accs = await AccountSchema.find({}).exec();
     
     if (accs.length > 0) {
-      const all_acc = accs.map(acc => ({
-        _id: acc._id,
-        email: acc.email,
-        f_name: acc.f_name,
-        m_name: acc.m_name,
-        l_name: acc.l_name,
-        address: {
-          brgy_name: acc.address.brgy_name,
-          municipality_name: acc.address.municipality_name
-        },
-        lgu_municipality: {
-          municipality_name: acc.lgu_municipality.municipality_name,
-          municipality_code: acc.lgu_municipality.municipality_code,
-          province_code: acc.lgu_municipality.province_code
-        },
-        img_name: acc.img_name,
-        user_type: acc.user_type,
-        verified: acc.verified
-      }));
-      
+      const all_acc = accs.map(acc => {
+        let img_url = null;
+        
+        if (acc.img_name) {
+          const imgPath = path.join(__dirname, '..', '..', 'client', 'public', 'img', 'user_img', `${acc.user_type}`, acc.img_name);
+          // Check if the file exists
+          if (fs.existsSync(imgPath)) {
+            img_url = `/img/user_img/${acc.user_type}/${acc.img_name}`;
+          }
+        }
+
+        return {
+          _id: acc._id,
+          email: acc.email,
+          f_name: acc.f_name,
+          m_name: acc.m_name,
+          l_name: acc.l_name,
+          address: {
+            brgy_name: acc.address.brgy_name,
+            municipality_name: acc.address.municipality_name
+          },
+          lgu_municipality: {
+            municipality_name: acc.lgu_municipality.municipality_name,
+            municipality_code: acc.lgu_municipality.municipality_code,
+            province_code: acc.lgu_municipality.province_code
+          },
+          img_name: acc.img_name,
+          img_url, // Include the image URL
+          user_type: acc.user_type,
+          verified: acc.verified
+        };
+      });
+
       return res.status(200).json(all_acc);
     }
 
