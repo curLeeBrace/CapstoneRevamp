318059546d23d196492cb3481550c5908ea5253e
CurLeeBrace <leeandreimain@gmail.com>
2024-09-17 10:09:57 +0800

    add industrial schema

---
+++ server/src/db_schema/Industrial/MetalSchema.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const MetalSchema = new mongoose.Schema({
+    survey_data : {
+        ispif : {type: Number}, //Iron and Steel Production from Integrated Facilities
+        ispnif : {type: Number}, // Iron and Steel Production from Non-integrated Facilities
+        brgy_name  :{type : String, required : true},
+        brgy_code : {type : String, required : true},
+        status : {type : String, required : true, default : "0"}
+    },
+
+    surveyor_info : {type :  {
+        email : {type : String, required : true},
+        full_name : {type : String, required : true},
+        municipality_name : {type : String, required : true},
+        municipality_code : {type : String, required : true},
+        province_code : {type : String, required : true},
+        img_id : {type : String, required : true}
+    }, required : true},
+
+    dateTime_created : {type : Date, required : true},
+    dateTime_edited : {type : Date}
+})
+
+export = mongoose.model('(industrial) metalForm', MetalSchema);
---
+++ server/src/db_schema/Industrial/OthersSchema.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const OthersSchema = new mongoose.Schema({
+    survey_data : {
+        ppi : {type : Number}, //Pulp and Paper Industry 
+        fbi : {type : Number}, //Food and Beverages Industry 
+        other : {type : Number}, //Other
+        brgy_name  :{type : String, required : true},
+        brgy_code : {type : String, required : true},
+        status : {type : String, required : true, default : "0"}
+    },
+
+    surveyor_info : {type :  {
+        email : {type : String, required : true},
+        full_name : {type : String, required : true},
+        municipality_name : {type : String, required : true},
+        municipality_code : {type : String, required : true},
+        province_code : {type : String, required : true},
+        img_id : {type : String, required : true}
+    }, required : true},
+
+    dateTime_created : {type : Date, required : true},
+    dateTime_edited : {type : Date}
+})
+
+export = mongoose.model('(industrial) metalForm', OthersSchema);
