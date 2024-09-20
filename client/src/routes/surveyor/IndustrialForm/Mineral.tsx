8e0f036f19d3c7a50aece9e88696dc5d791f6100
CurLeeBrace <leeandreimain@gmail.com>
2024-09-20 13:42:49 +0800

    modify industrial form

--- client/src/routes/surveyor/IndustrialForm/Mineral.tsx
+++ client/src/routes/surveyor/IndustrialForm/Mineral.tsx
@@ -2,49 +2,50 @@ import { Typography, Input } from "@material-tailwind/react"
 
 const Mineral = () => {
     return(
-        <div className="relative">
-        <div className="relative mb-4 w-2/3 whitespace-nowrap">
-          <Typography className="-mb-4 text-md">
+        <div className="flex justify-around flex-wrap">
+
+        <div className="w-full lg:w-2/5 flex flex-col gap-3">
+          <Typography className="">
             Cement Production - Portland (tons)
           </Typography>
           <Input
             size="lg"
             type="number"
             placeholder="0"
-            className="!border-t-blue-gray-200 focus:!border-t-gray-900 mt-4"
+            className="!border-t-blue-gray-200 focus:!border-t-gray-900"
             labelProps={{
               className: "before:content-none after:content-none",
             }}
           />
-          <Typography className="-mb-4 text-md mt-10">
+          <Typography className="">
             Cement Production - Portland (blended)
           </Typography>
           <Input
             size="lg"
             type="number"
             placeholder="0"
-            className="!border-t-blue-gray-200 focus:!border-t-gray-900 mt-4"
+            className="!border-t-blue-gray-200 focus:!border-t-gray-900 "
             labelProps={{
               className: "before:content-none after:content-none",
             }}
           />
         </div>
 
-        <div className="md:absolute top-0 left-3/4 md:ml-28 w-2/3 flex flex-col">
-          <Typography className="-mb-4 text-md">
+        <div className="w-full lg:w-2/5 flex flex-col gap-3">
+          <Typography>
             Lime Production (tons)
           </Typography>
           <Input
             size="lg"
             type="number"
             placeholder="0"
-            className="!border-t-blue-gray-200 focus:!border-t-gray-900 mt-4"
+            className="!border-t-blue-gray-200 focus:!border-t-gray-900"
             labelProps={{
               className: "before:content-none after:content-none",
             }}
           />
 
-          <Typography className=" text-md mt-10 ">
+          <Typography>
             Glass Production (tons)
           </Typography>
           <Input
--- client/src/routes/surveyor/IndustrialForm/Others.tsx
+++ client/src/routes/surveyor/IndustrialForm/Others.tsx
@@ -1,46 +1,46 @@
 import { Typography, Input } from "@material-tailwind/react"
 
-
-
 const Others = () => {
     return (
-        <div className="relative">
-                  <div className="relative mb-4 w-2/3 whitespace-nowrap">
-                    <Typography className="-mb-4 text-md">
+        <div className="flex flex-wrap justify-around">
+                  <div className="w-full lg:w-2/5 flex flex-col gap-3">
+                    <Typography>
                       Pulp and paper industry (tons)
                     </Typography>
                     <Input
                       size="lg"
                       type="number"
                       placeholder="0"
-                      className="!border-t-blue-gray-200 focus:!border-t-gray-900 mt-4"
+                      className="!border-t-blue-gray-200 focus:!border-t-gray-900"
                       labelProps={{
                         className: "before:content-none after:content-none",
                       }}
                     />
-                    <Typography className="-mb-4 text-md mt-10">
+                    <Typography>
                       Food and beverages industry (tons)
                     </Typography>
                     <Input
                       size="lg"
                       type="number"
                       placeholder="0"
-                      className="!border-t-blue-gray-200 focus:!border-t-gray-900 mt-4"
+                      className="!border-t-blue-gray-200 focus:!border-t-gray-900"
                       labelProps={{
                         className: "before:content-none after:content-none",
                       }}
                     />
                   </div>
 
-                  <div className="md:absolute top-0 left-3/4 md:ml-28 w-2/3 flex flex-col">
-                    <Typography className="-mb-4 text-md">
+
+
+                  <div className="w-full lg:w-2/5 flex flex-col gap-3">
+                    <Typography>
                       Other carbon in pulp (tons)
                     </Typography>
                     <Input
                       size="lg"
                       type="number"
                       placeholder="0"
-                      className="!border-t-blue-gray-200 focus:!border-t-gray-900 mt-4"
+                      className="!border-t-blue-gray-200 focus:!border-t-gray-900"
                       labelProps={{
                         className: "before:content-none after:content-none",
                       }}
@@ -48,6 +48,7 @@ const Others = () => {
                   </div>
                 </div>
     )
+
 }
 
 
