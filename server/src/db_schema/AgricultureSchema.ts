4d3afeb0142ad21acc8f947163b7637b5e6dc4a1
CurLeeBrace <leeandreimain@gmail.com>
2024-10-05 09:58:38 +0800

    add agriculture submit function

---
+++ server/src/db_schema/AgricultureSchema.ts
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose'
+
+
+const AgricultureForm = new mongoose.Schema({
+    survey_data : { type : {
+
+        // form_type : {type : String, required : true},
+        crops : {type:{
+            rdsi : {type:Number, required : true}, //Rice (Dry Season, Irrigated)
+            rdsr : {type:Number, required : true}, //Rice (Dry Season, Rainfed)
+            rwsi : {type:Number, required : true}, //Rice (Wet Season, Irrigated)
+            rwsr : {type:Number, required : true}, //Rice (Wet Season, Rainfed)
+            crop_residues : {type:Number, required : true}, // Crop Residues (tonnes of dry weight)
+            dol_limestone : {type:Number, required : true},//Dolomite and/or Limestone Consumption
+        },required : true},
+
+        live_stock :{type : {
+            buffalo  : {type : Number, required : true},//
+            cattle : {type : Number, required : true},//
+            goat : {type : Number, required : true},//
+            horse : {type : Number, required : true},//
+            poultry : {type : Number, required : true},//
+            swine : {type : Number, required : true},//
+            non_dairyCattle :{type : Number, required : true}
+
+        }, required : true},
+ 
+
+
+
+
+        
+        brgy_name  :{type : String, required : true},
+        brgy_code : {type : String, required : true},
+        status : {type : String, required : true, default : "0"}
+        },required : true
+    },
+
+    surveyor_info : {type :  {
+        email : {type : String, required : true},
+        full_name : {type : String, required : true},
+        municipality_name : {type : String, required : true},
+        municipality_code : {type : String, required : true},
+        province_code : {type : String, required : true},
+        img_id : {type : String, required : true}
+    }, required : true},
+
+    dateTime_created : {type : Date, required : true},
+    dateTime_edited : {type : Date}
+})
+
+
+export = mongoose.model('agricultureForm', AgricultureForm);
